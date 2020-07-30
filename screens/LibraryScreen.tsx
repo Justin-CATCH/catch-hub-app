042cd5f1a5d9116827b24d35cd61ec93c4a1fb0e
Lionel Tay <lionel.lt.tay@gmail.com>
2020-07-30 15:11:17 +1000

    add library

--- screens/LibraryScreen.tsx
+++ screens/LibraryScreen.tsx
@@ -1,30 +1,80 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet, ScrollView, Button } from "react-native";
+import Markdown from "react-native-markdown-renderer";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
 
 export default function LibraryScreen() {
+  const [data, setData] = React.useState("");
+  const [files, setFiles] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      "https://api.docs.cgws.com.au/services/knowledge-base/branches/catch-hub-app"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setFiles(data.branch.files);
+      });
+  }, []);
+
+  if (data) {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <View>
+            <Button
+              onPress={() => {
+                setData(null);
+              }}
+              title="Back"
+            />
+          </View>
+          <Markdown>{data}</Markdown>
+        </View>
+      </ScrollView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Library Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {files.map((file) => (
+          <View key={file.path}>
+            <Text
+              onPress={() => {
+                console.log("click");
+                fetch(file._links.content)
+                  .then((res) => res.text())
+                  .then((md) => {
+                    setData(md);
+                  });
+              }}
+            >
+              {file.name}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 8,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
