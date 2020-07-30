45eb28a94146bdade9b5f3d9404e524e1ad040ae
Lionel Tay <lionel.lt.tay@gmail.com>
2020-07-30 16:47:59 +1000

    onboarding data

--- mock-data/index.ts
+++ mock-data/index.ts
@@ -113,9 +113,71 @@ export const MOCK_LIBRARY_DATA = [
 export const ONBOARDING_MOCK_DATA = {
   frontend: {
     title: "Frontend",
+    days: [
+      {
+        description:
+          "Welcome to your first day at Catch! We hope that this will be both a pleasant and exciting place to work. Today you will:",
+        tasks: [
+          {
+            text:
+              "Get the morning coffee, checkout our cafe and office as well as a quick chat with your line manager.",
+          },
+          {
+            text:
+              'Meet your "onboarding buddy", the goto person that will join you during your onboarding journey.',
+          },
+          { text: "Meet some the people you will be working with." },
+          { text: "Get your free Club Catch account." },
+          { text: "Set up your OneLogin, Slack, JIRA... etc accounts." },
+          { text: "Familiarise yourself with Catch policies." },
+        ],
+      },
+      {
+        description:
+          "Today you will start on your engineering journey at Catch:",
+        tasks: [
+          {
+            text: "Get a brief understanding about Catch general architecture",
+          },
+          { text: "Set up your local machine" },
+          { text: "Learn about our Git, commit and branching strategy" },
+        ],
+      },
+      {
+        description:
+          "Today you will deep dive on how we do frontend development at Catch.",
+        tasks: [
+          { text: "Learn about our frontend architecture" },
+          { text: "Learn about our frontend tech stack" },
+          { text: "Learn about our testing strategies" },
+          { text: "Learn about our Design System" },
+          { text: "Learn about our component library" },
+        ],
+      },
+      {
+        description: `Today, let's start seeing the code and making Catch frontend accessible on your local machine 💪.`,
+        tasks: [
+          { text: "Set up your frontend monorepo" },
+          { text: "Learn about Web and Unity" },
+          { text: "Learn about RBT and Absinthe" },
+          { text: "Set up your Web and Unity environments" },
+        ],
+      },
 
-  }
-}
+      {
+        description: `'Let's get your hand dirty today.`,
+        tasks: [
+          {
+            text:
+              "Tackle your first ticket, on your own or pairing with your onboarding buddy",
+          },
+          { text: "Review one Pull Request" },
+        ],
+        conclusion: `Congrats, you have completed your first week at Catch. If you are able to finish all of your tasks, you are a champion, please see Justin to claim your beer/coffee (preferably not both 😝).`,
+      },
+    ],
+  },
+};
 
 // export default function LibraryScreen() {
 //   const [data, setData] = React.useState("");
