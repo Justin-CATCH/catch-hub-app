a780b25168868ded75bb8ca127ea8eb9c789c37f
Justin Nguyen <justin.nguyen@catch.com.au>
2020-07-30 11:24:08 +1000

    chore: add Firebase DB

---
+++ db/index.ts
@@ -0,0 +1,18 @@
+import * as firebase from "firebase";
+import "firebase/database";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDD6Rv7qcG-RH1D-ABcRFBu1ifDnJQdp2E",
+  authDomain: "catchhubapp.firebaseapp.com",
+  databaseURL: "https://catchhubapp.firebaseio.com",
+  projectId: "catchhubapp",
+  storageBucket: "catchhubapp.appspot.com",
+  messagingSenderId: "1006264468393",
+  appId: "1:1006264468393:web:bcd3cbe22278a0a37cbab8",
+  measurementId: "G-3ZNNMN2055",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const dbInstance = firebase.firestore();
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
 
 export default function HomeScreen() {
   return (
@@ -14,16 +13,16 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
